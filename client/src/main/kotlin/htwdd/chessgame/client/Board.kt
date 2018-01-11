867b9c671c4ae55e11de8a6741e2529f5d463919
GagaMen <felixdimmel93@gmail.com>
2018-01-11 23:40:54 +0100

    Update model classes

--- client/src/main/kotlin/htwdd/chessgame/client/Board.kt
+++
@@ -1,5 +0,0 @@
-package htwdd.chessgame.client
-
-class Board(var id: Int,
-            var pieceSets: Map<PieceColor, PieceSet>,
-            var boardCode: String)
--- client/src/main/kotlin/htwdd/chessgame/client/Match.kt
+++ client/src/main/kotlin/htwdd/chessgame/client/Match.kt
@@ -2,5 +2,6 @@ package htwdd.chessgame.client
 
 class Match(var id: Int,
             var players: Map<PieceColor, Player>,
-            var board: Board,
-            var history: Array<Draw>)
+            var pieceSets: Map<PieceColor, PieceSet>,
+            var history: Array<Draw>,
+            var matchCode: String)
