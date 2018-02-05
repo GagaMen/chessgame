7aabc720a3f3bbee3745a2bfe757e45412285352
GagaMen <felixdimmel93@gmail.com>
2018-02-05 14:45:12 +0100

    Refactor rules into own functions + add queen movement rule

--- client/src/main/kotlin/htwdd/chessgame/client/util/ChessRuleUtility.kt
+++ client/src/main/kotlin/htwdd/chessgame/client/util/ChessRuleUtility.kt
@@ -9,8 +9,9 @@ import kotlin.dom.hasClass
 
 class ChessRuleUtility {
     companion object {
+        private val validDropFields = HashSet<Pair<Int, Int>>()
+
         fun getValidDropFields(image: Element): HashSet<Pair<Int, Int>> {
-            val validDropFields = HashSet<Pair<Int, Int>>()
             var pieceColor = PieceColor.WHITE
             val type = image.attributes["data-type"]?.nodeValue
             val parent = image.parentElement
@@ -25,170 +26,183 @@ class ChessRuleUtility {
 
             if (row != null && col != null) {
                 when (type) {
-                    PieceType.BISHOP.toString() -> {
-                        //top
-                        if (row != 1) {
-                            //left
-                            if (col != 1) {
-                                var tmpCol = col
-                                for (i in row - 1 downTo 1) {
-                                    tmpCol--
-                                    if (tmpCol != 0) { // out of board
-                                        val field = document.getElementById("board--field-$i-$tmpCol")
-                                        if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
-                                        else break
-                                    } else {
-                                        break
-                                    }
-                                }
-                            }
-                            //right
-                            if (col != 8) {
-                                var tmpCol = col
-                                for (i in row - 1 downTo 1) {
-                                    tmpCol++
-                                    if (tmpCol != 9) { // out of board
-                                        val field = document.getElementById("board--field-$i-$tmpCol")
-                                        if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
-                                        else break
-                                    } else {
-                                        break
-                                    }
-                                }
-                            }
-                        }
+                    PieceType.BISHOP.toString() -> bishop(row, col)
+                    PieceType.KING.toString() -> king(row, col)
+                    PieceType.KNIGHT.toString() -> knight(row, col)
+                    PieceType.PAWN.toString() -> pawn(row, col, pieceColor)
+                    PieceType.QUEEN.toString() -> queen(row, col)
+                    PieceType.ROOK.toString() -> rook(row, col)
+                }
+            }
 
-                        //bottom
-                        if (row != 8) {
-                            //left
-                            if (col != 1) {
-                                var tmpCol = col
-                                for (i in row + 1..8) {
-                                    tmpCol--
-                                    if (tmpCol != 0) { // out of board
-                                        val field = document.getElementById("board--field-$i-$tmpCol")
-                                        if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
-                                        else break
-                                    } else {
-                                        break
-                                    }
-                                }
-                            }
-                            //right
-                            if (col != 8) {
-                                var tmpCol = col
-                                for (i in row + 1..8) {
-                                    tmpCol++
-                                    if (tmpCol != 9) { // out of board
-                                        val field = document.getElementById("board--field-$i-$tmpCol")
-                                        if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
-                                        else break
-                                    } else {
-                                        break
-                                    }
-                                }
-                            }
+            return validDropFields
+        }
+
+        private fun bishop(row: Int, col: Int) {
+            //top
+            if (row != 1) {
+                //left
+                if (col != 1) {
+                    var tmpCol = col
+                    for (i in row - 1 downTo 1) {
+                        tmpCol--
+                        if (tmpCol != 0) { // out of board
+                            val field = document.getElementById("board--field-$i-$tmpCol")
+                            if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
+                            else break
+                        } else {
+                            break
                         }
                     }
-                    PieceType.KING.toString() -> {
-                        val field1 = document.getElementById("board--field-${row + 1}-$col")
-                        val field2 = document.getElementById("board--field-${row + 1}-${col + 1}")
-                        val field3 = document.getElementById("board--field-$row-${col + 1}")
-                        val field4 = document.getElementById("board--field-${row - 1}-${col + 1}")
-                        val field5 = document.getElementById("board--field-${row - 1}-$col")
-                        val field6 = document.getElementById("board--field-${row - 1}-${col - 1}")
-                        val field7 = document.getElementById("board--field-$row-${col - 1}")
-                        val field8 = document.getElementById("board--field-${row + 1}-${col - 1}")
-
-                        if (field1 != null && !field1.hasChildNodes()) validDropFields.add(Pair(row + 1, col))
-                        if (field2 != null && !field2.hasChildNodes()) validDropFields.add(Pair(row + 1, col + 1))
-                        if (field3 != null && !field3.hasChildNodes()) validDropFields.add(Pair(row, col + 1))
-                        if (field4 != null && !field4.hasChildNodes()) validDropFields.add(Pair(row - 1, col + 1))
-                        if (field5 != null && !field5.hasChildNodes()) validDropFields.add(Pair(row - 1, col))
-                        if (field6 != null && !field6.hasChildNodes()) validDropFields.add(Pair(row - 1, col - 1))
-                        if (field7 != null && !field7.hasChildNodes()) validDropFields.add(Pair(row, col - 1))
-                        if (field8 != null && !field8.hasChildNodes()) validDropFields.add(Pair(row + 1, col - 1))
-                    }
-                    PieceType.KNIGHT.toString() -> {
-                        val field1 = document.getElementById("board--field-${row + 2}-${col + 1}")
-                        val field2 = document.getElementById("board--field-${row + 1}-${col + 2}")
-                        val field3 = document.getElementById("board--field-${row - 1}-${col + 2}")
-                        val field4 = document.getElementById("board--field-${row - 2}-${col + 1}")
-                        val field5 = document.getElementById("board--field-${row - 2}-${col - 1}")
-                        val field6 = document.getElementById("board--field-${row - 1}-${col - 2}")
-                        val field7 = document.getElementById("board--field-${row + 1}-${col - 2}")
-                        val field8 = document.getElementById("board--field-${row + 2}-${col - 1}")
-
-                        if (field1 != null && !field1.hasChildNodes()) validDropFields.add(Pair(row + 2, col + 1))
-                        if (field2 != null && !field2.hasChildNodes()) validDropFields.add(Pair(row + 1, col + 2))
-                        if (field3 != null && !field3.hasChildNodes()) validDropFields.add(Pair(row - 1, col + 2))
-                        if (field4 != null && !field4.hasChildNodes()) validDropFields.add(Pair(row - 2, col + 1))
-                        if (field5 != null && !field5.hasChildNodes()) validDropFields.add(Pair(row - 2, col - 1))
-                        if (field6 != null && !field6.hasChildNodes()) validDropFields.add(Pair(row - 1, col - 2))
-                        if (field7 != null && !field7.hasChildNodes()) validDropFields.add(Pair(row + 1, col - 2))
-                        if (field8 != null && !field8.hasChildNodes()) validDropFields.add(Pair(row + 2, col - 1))
-                    }
-                    PieceType.PAWN.toString() -> {
-                        if (pieceColor == PieceColor.WHITE) {
-                            val field = document.getElementById("board--field-${row + 1}-$col")
-                            if (field != null && !field.hasChildNodes()) {
-                                validDropFields.add(Pair(row + 1, col))
-                                // start row
-                                if (row == 2) validDropFields.add(Pair(row + 2, col))
-                            }
-                        }
-
-                        if (pieceColor == PieceColor.BLACK) {
-                            val field = document.getElementById("board--field-${row - 1}-$col")
-                            if (field != null && !field.hasChildNodes()) {
-                                validDropFields.add(Pair(row - 1, col))
-                                // start row
-                                if (row == 7) validDropFields.add(Pair(row - 2, col))
-                            }
-                        }
-
-                    }
-                    PieceType.QUEEN.toString() -> {
-                    }
-                    PieceType.ROOK.toString() -> {
-                        //horizontal left
-                        if (col != 1) {
-                            for (i in (col - 1 downTo 1)) {
-                                val field = document.getElementById("board--field-$row-$i")
-                                if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(row, i))
-                                else break
-                            }
-                        }
-                        //horizontal right
-                        if (col != 8) {
-                            for (i in (col + 1..8)) {
-                                val field = document.getElementById("board--field-$row-$i")
-                                if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(row, i))
-                                else break
-                            }
-                        }
-
-                        //vertical top
-                        if (row != 1) {
-                            for (i in (row - 1 downTo 1)) {
-                                val field = document.getElementById("board--field-$i-$col")
-                                if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, col))
-                                else break
-                            }
-                        }
-                        //vertical bottom
-                        if (row != 8) {
-                            for (i in (row + 1..8)) {
-                                val field = document.getElementById("board--field-$i-$col")
-                                if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, col))
-                                else break
-                            }
+                }
+                //right
+                if (col != 8) {
+                    var tmpCol = col
+                    for (i in row - 1 downTo 1) {
+                        tmpCol++
+                        if (tmpCol != 9) { // out of board
+                            val field = document.getElementById("board--field-$i-$tmpCol")
+                            if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
+                            else break
+                        } else {
+                            break
                         }
                     }
                 }
             }
 
-            return validDropFields
+            //bottom
+            if (row != 8) {
+                //left
+                if (col != 1) {
+                    var tmpCol = col
+                    for (i in row + 1..8) {
+                        tmpCol--
+                        if (tmpCol != 0) { // out of board
+                            val field = document.getElementById("board--field-$i-$tmpCol")
+                            if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
+                            else break
+                        } else {
+                            break
+                        }
+                    }
+                }
+                //right
+                if (col != 8) {
+                    var tmpCol = col
+                    for (i in row + 1..8) {
+                        tmpCol++
+                        if (tmpCol != 9) { // out of board
+                            val field = document.getElementById("board--field-$i-$tmpCol")
+                            if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, tmpCol))
+                            else break
+                        } else {
+                            break
+                        }
+                    }
+                }
+            }
+        }
+
+        private fun king(row: Int, col: Int) {
+            val field1 = document.getElementById("board--field-${row + 1}-$col")
+            val field2 = document.getElementById("board--field-${row + 1}-${col + 1}")
+            val field3 = document.getElementById("board--field-$row-${col + 1}")
+            val field4 = document.getElementById("board--field-${row - 1}-${col + 1}")
+            val field5 = document.getElementById("board--field-${row - 1}-$col")
+            val field6 = document.getElementById("board--field-${row - 1}-${col - 1}")
+            val field7 = document.getElementById("board--field-$row-${col - 1}")
+            val field8 = document.getElementById("board--field-${row + 1}-${col - 1}")
+
+            if (field1 != null && !field1.hasChildNodes()) validDropFields.add(Pair(row + 1, col))
+            if (field2 != null && !field2.hasChildNodes()) validDropFields.add(Pair(row + 1, col + 1))
+            if (field3 != null && !field3.hasChildNodes()) validDropFields.add(Pair(row, col + 1))
+            if (field4 != null && !field4.hasChildNodes()) validDropFields.add(Pair(row - 1, col + 1))
+            if (field5 != null && !field5.hasChildNodes()) validDropFields.add(Pair(row - 1, col))
+            if (field6 != null && !field6.hasChildNodes()) validDropFields.add(Pair(row - 1, col - 1))
+            if (field7 != null && !field7.hasChildNodes()) validDropFields.add(Pair(row, col - 1))
+            if (field8 != null && !field8.hasChildNodes()) validDropFields.add(Pair(row + 1, col - 1))
+        }
+
+        private fun knight(row: Int, col: Int) {
+            val field1 = document.getElementById("board--field-${row + 2}-${col + 1}")
+            val field2 = document.getElementById("board--field-${row + 1}-${col + 2}")
+            val field3 = document.getElementById("board--field-${row - 1}-${col + 2}")
+            val field4 = document.getElementById("board--field-${row - 2}-${col + 1}")
+            val field5 = document.getElementById("board--field-${row - 2}-${col - 1}")
+            val field6 = document.getElementById("board--field-${row - 1}-${col - 2}")
+            val field7 = document.getElementById("board--field-${row + 1}-${col - 2}")
+            val field8 = document.getElementById("board--field-${row + 2}-${col - 1}")
+
+            if (field1 != null && !field1.hasChildNodes()) validDropFields.add(Pair(row + 2, col + 1))
+            if (field2 != null && !field2.hasChildNodes()) validDropFields.add(Pair(row + 1, col + 2))
+            if (field3 != null && !field3.hasChildNodes()) validDropFields.add(Pair(row - 1, col + 2))
+            if (field4 != null && !field4.hasChildNodes()) validDropFields.add(Pair(row - 2, col + 1))
+            if (field5 != null && !field5.hasChildNodes()) validDropFields.add(Pair(row - 2, col - 1))
+            if (field6 != null && !field6.hasChildNodes()) validDropFields.add(Pair(row - 1, col - 2))
+            if (field7 != null && !field7.hasChildNodes()) validDropFields.add(Pair(row + 1, col - 2))
+            if (field8 != null && !field8.hasChildNodes()) validDropFields.add(Pair(row + 2, col - 1))
+        }
+
+        private fun pawn(row: Int, col: Int, pieceColor: PieceColor) {
+            if (pieceColor == PieceColor.WHITE) {
+                val field = document.getElementById("board--field-${row + 1}-$col")
+                if (field != null && !field.hasChildNodes()) {
+                    validDropFields.add(Pair(row + 1, col))
+                    // start row
+                    if (row == 2) validDropFields.add(Pair(row + 2, col))
+                }
+            }
+
+            if (pieceColor == PieceColor.BLACK) {
+                val field = document.getElementById("board--field-${row - 1}-$col")
+                if (field != null && !field.hasChildNodes()) {
+                    validDropFields.add(Pair(row - 1, col))
+                    // start row
+                    if (row == 7) validDropFields.add(Pair(row - 2, col))
+                }
+            }
+        }
+
+        private fun queen(row: Int, col: Int) {
+            bishop(row, col)
+            rook(row, col)
+        }
+
+        private fun rook(row: Int, col: Int) {
+            //horizontal left
+            if (col != 1) {
+                for (i in (col - 1 downTo 1)) {
+                    val field = document.getElementById("board--field-$row-$i")
+                    if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(row, i))
+                    else break
+                }
+            }
+            //horizontal right
+            if (col != 8) {
+                for (i in (col + 1..8)) {
+                    val field = document.getElementById("board--field-$row-$i")
+                    if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(row, i))
+                    else break
+                }
+            }
+
+            //vertical top
+            if (row != 1) {
+                for (i in (row - 1 downTo 1)) {
+                    val field = document.getElementById("board--field-$i-$col")
+                    if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, col))
+                    else break
+                }
+            }
+            //vertical bottom
+            if (row != 8) {
+                for (i in (row + 1..8)) {
+                    val field = document.getElementById("board--field-$i-$col")
+                    if (field != null && !field.hasChildNodes()) validDropFields.add(Pair(i, col))
+                    else break
+                }
+            }
         }
     }
 }
