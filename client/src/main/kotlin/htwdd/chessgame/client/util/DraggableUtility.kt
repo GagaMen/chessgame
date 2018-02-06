f267c9a77ebd369e9ca57eb4d69d9d24d6099f71
GagaMen <felixdimmel93@gmail.com>
2018-02-06 14:00:50 +0100

    Move calculation of valid fields into draggable utility class

--- client/src/main/kotlin/htwdd/chessgame/client/util/DraggableUtility.kt
+++ client/src/main/kotlin/htwdd/chessgame/client/util/DraggableUtility.kt
@@ -1,14 +1,23 @@
 package htwdd.chessgame.client.util
 
+import htwdd.chessgame.client.model.PieceColor
+import htwdd.chessgame.client.model.PieceType
 import org.w3c.dom.*
 import org.w3c.dom.events.Event
 import kotlin.browser.document
 import kotlin.dom.addClass
+import kotlin.dom.hasClass
 import kotlin.dom.removeClass
 
 class DraggableUtility {
     companion object {
-        var validDropFields = HashSet<Pair<Int, Int>>()
+        private var validDropFields = HashSet<Pair<Int, Int>>()
+        private val bishop = BishopMovementUtility()
+        private val king = KingMovementUtility()
+        private val knight = KnightMovementUtility()
+        private val pawn = PawnMovementUtility()
+        private val queen = QueenMovementUtility()
+        private val rook = RookMovementUtility()
 
         fun dragStart(event: Event) {
             when (event) {
@@ -16,7 +25,7 @@ class DraggableUtility {
                     val target = event.target
                     if (target is Element) {
                         if (validDropFields.size == 0) {
-                            validDropFields = ChessRuleUtility.getValidDropFields(target)
+                            calculateValidDropFields(target)
                         }
                         validDropFields.forEach {
                             val field = document.getElementById("board--field-${it.first}-${it.second}")
@@ -67,7 +76,7 @@ class DraggableUtility {
             val target = event.target
             when (target) {
                 is Element -> {
-                    validDropFields = ChessRuleUtility.getValidDropFields(target)
+                    calculateValidDropFields(target)
                     validDropFields.forEach {
                         val field = document.getElementById("board--field-${it.first}-${it.second}")
                         field?.addClass("highlighted")
@@ -85,6 +94,31 @@ class DraggableUtility {
                         field?.removeClass("highlighted")
                     }
                     validDropFields.clear()
+                }
+            }
+        }
+
+        fun calculateValidDropFields(image: Element) {
+            var pieceColor = PieceColor.WHITE
+            val type = image.attributes["data-type"]?.nodeValue
+            val parent = image.parentElement
+            val row = parent!!.attributes["data-row"]?.nodeValue?.toIntOrNull()
+            val col = parent.attributes["data-col"]?.nodeValue?.toIntOrNull()
+
+            if (image.hasClass("piece--black")) {
+                pieceColor = PieceColor.BLACK
+            } else if (!image.hasClass("piece--white")) {
+                // todo throw error
+            }
+
+            if (row != null && col != null) {
+                when (type) {
+                    PieceType.BISHOP.toString() -> bishop.setValidDropFields(validDropFields, row, col, pieceColor)
+                    PieceType.KING.toString() -> king.setValidDropFields(validDropFields, row, col, pieceColor)
+                    PieceType.KNIGHT.toString() -> knight.setValidDropFields(validDropFields, row, col, pieceColor)
+                    PieceType.PAWN.toString() -> pawn.setValidDropFields(validDropFields, row, col, pieceColor)
+                    PieceType.QUEEN.toString() -> queen.setValidDropFields(validDropFields, row, col, pieceColor)
+                    PieceType.ROOK.toString() -> rook.setValidDropFields(validDropFields, row, col, pieceColor)
                 }
             }
         }
