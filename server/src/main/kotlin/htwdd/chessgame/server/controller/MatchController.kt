77ec414e13b9897ea792f7eb15755b70a243a0a5
GagaMen <felixdimmel93@gmail.com>
2018-02-28 12:59:21 +0100

    Add SQLite connection to match controller + add functionality for restoring data from match code

--- server/src/main/kotlin/htwdd/chessgame/server/controller/MatchController.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/controller/MatchController.kt
@@ -1,45 +1,73 @@
 package htwdd.chessgame.server.controller
 
 import htwdd.chessgame.server.model.Match
-import htwdd.chessgame.server.model.PieceColor
 import htwdd.chessgame.server.model.Player
+import ninja.sakib.pultusorm.core.PultusORM
+import ninja.sakib.pultusorm.core.PultusORMCondition
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class MatchController {
-    private final val matchList: HashMap<Int, Match> = HashMap()
+    private val pultusORM: PultusORM = PultusORM("chessgame.db")
 
     @GetMapping("/match")
     fun getMatchList(): HashMap<Int, Match> {
+        val matchList = HashMap<Int, Match>()
+
+        pultusORM.find(Match())
+                .filterIsInstance<Match>()
+                .forEach { it ->
+                    it.setValuesByMatchCode()
+                    matchList[it.id] = it
+                }
+
         return matchList
     }
 
     @GetMapping("/match/{id}")
     fun getMatchById(@PathVariable id: Int): Any? {
-        if (matchList.containsKey(id)) return matchList[id]
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", id)
+                .build()
+
+        pultusORM.find(Match(), condition)
+                .filterIsInstance<Match>()
+                .forEach { it ->
+                    it.setValuesByMatchCode()
+                    return it
+                }
+
         return "No match with id \"$id\" registered!"
     }
 
     @DeleteMapping("/match/{id}")
     fun deleteMatchById(@PathVariable id: Int): Boolean {
-        if (matchList.containsKey(id)) {
-            matchList.remove(id)
-            return true
-        }
-        return false
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", id)
+                .build()
+
+        return pultusORM.delete(Match(), condition)
     }
 
-    @PutMapping("/match/{id}")
-    fun addMatch(@PathVariable id: Int,
-                 @RequestParam playerWhite: Player,
-                 @RequestParam playerBlack: Player): Boolean {
-        if (!matchList.containsKey(id)) {
-            val players: HashMap<PieceColor, Player> = HashMap()
-            players[PieceColor.WHITE] = playerWhite
-            players[PieceColor.BLACK] = playerBlack
-            matchList[id] = Match(id, players)
-            return true
-        }
-        return false
+    @PutMapping("/match")
+    fun addMatch(@RequestParam playerWhiteId: Int,
+                 @RequestParam playerBlackId: Int): Boolean {
+        var playerWhite: Player? = null
+        var playerBlack: Player? = null
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", playerWhiteId)
+                .or()
+                .eq("ID", playerBlackId)
+                .build()
+
+        pultusORM.find(Player(), condition)
+                .filterIsInstance<Player>()
+                .forEach { it ->
+                    if (it.id == playerWhiteId) playerWhite = it
+                    if (it.id == playerBlackId) playerBlack = it
+                }
+
+        if (playerWhite == null || playerBlack == null) return false
+        return pultusORM.save(Match(playerWhiteId, playerBlackId))
     }
 }
