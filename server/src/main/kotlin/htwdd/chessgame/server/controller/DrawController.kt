da7365c655f1fd9d4a18b7ed6f536358a2b02d05
GagaMen <felixdimmel93@gmail.com>
2018-03-02 11:57:04 +0100

    Add draw controller with basic functions + optimize draw class

---
+++ server/src/main/kotlin/htwdd/chessgame/server/controller/DrawController.kt
@@ -0,0 +1,83 @@
+package htwdd.chessgame.server.controller
+
+import htwdd.chessgame.server.model.Draw
+import htwdd.chessgame.server.model.Match
+import ninja.sakib.pultusorm.core.PultusORM
+import ninja.sakib.pultusorm.core.PultusORMCondition
+import org.springframework.web.bind.annotation.*
+
+@RestController
+class DrawController {
+    private val pultusORM: PultusORM = PultusORM("chessgame.db")
+
+    @GetMapping("draw")
+    fun getDrawList(): MutableList<Draw> {
+        val drawList: MutableList<Draw> = mutableListOf()
+
+        pultusORM.find(Draw())
+                .filterIsInstance<Draw>()
+                .forEach { draw ->
+                    draw.setValuesByDrawCode()
+                    drawList.add(draw)
+                }
+
+        return drawList
+    }
+
+    @GetMapping("draw/{id}")
+    fun getDrawById(@PathVariable id: Int): Any {
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", id)
+                .build()
+
+        pultusORM.find(Draw(), condition)
+                .filterIsInstance<Draw>()
+                .forEach { draw ->
+                    draw.setValuesByDrawCode()
+                    return draw
+                }
+
+        return "No draw with id \"$id\" registered!"
+    }
+
+    @DeleteMapping("draw/{id}")
+    fun deleteDrawById(@PathVariable id: Int): Boolean {
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", id)
+                .build()
+
+        return pultusORM.delete(Draw(), condition)
+    }
+
+    @PutMapping("draw")
+    fun addDraw(@RequestParam matchId: Int,
+                @RequestParam color: String,
+                @RequestParam type: String,
+                @RequestParam startRow: Int,
+                @RequestParam startCol: Int,
+                @RequestParam endRow: Int,
+                @RequestParam endCol: Int,
+                @RequestParam drawCode: String): Boolean {
+        var match: Match? = null
+        val condition = PultusORMCondition.Builder()
+                .eq("ID", matchId)
+                .build()
+
+        pultusORM.find(Match(), condition)
+                .filterIsInstance<Match>()
+                .forEach { it ->
+                    if (it.id == matchId) match = it
+                }
+
+        if (match == null) return false
+
+        return pultusORM.save(Draw(colorString = color,
+                pieceTypeString = type,
+                startRow = startRow,
+                startColumn = startCol,
+                endRow = endRow,
+                endColumn = endCol,
+                matchID = matchId,
+                drawCode = drawCode))
+    }
+}
--- server/src/main/kotlin/htwdd/chessgame/server/model/Draw.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/model/Draw.kt
@@ -1,20 +1,39 @@
 package htwdd.chessgame.server.model
 
 import ninja.sakib.pultusorm.annotations.AutoIncrement
+import ninja.sakib.pultusorm.annotations.Ignore
 import ninja.sakib.pultusorm.annotations.PrimaryKey
 
-data class Draw(var color: String = "",
-                var pieceType: String = "",
+data class Draw(var colorString: String = "",
+                @Ignore var color: PieceColor? = null,
+                var pieceTypeString: String = "",
+                @Ignore var pieceType: PieceType? = null,
                 var startRow: Int = -1,
                 var startColumn: Int = -1,
+                @Ignore var start: Field? = null,
                 var endRow: Int = -1,
                 var endColumn: Int = -1,
-                var throwPiece: Boolean = false,
-                var throwEnPassant: Boolean = false,
-                var kingsideCastling: Boolean = false,
-                var queensideCastling: Boolean = false,
+                @Ignore var end: Field? = null,
+                var matchID: Int = -1,
+                @Ignore var throwPiece: Boolean = false,
+                @Ignore var throwEnPassant: Boolean = false,
+                @Ignore var kingsideCastling: Boolean = false,
+                @Ignore var queensideCastling: Boolean = false,
                 var drawCode: String = "") {
     @PrimaryKey
     @AutoIncrement
     var id: Int = 0
+
+    fun setValuesByDrawCode() {
+        if (drawCode.contains("x")) throwPiece = true
+        if (drawCode.contains("e.p.")) throwEnPassant = true
+        if (drawCode.contains("O-O")) kingsideCastling = true
+        if (drawCode.contains("O-O-O")) queensideCastling = true
+
+        color = PieceColor.valueOf(colorString)
+        pieceType = PieceType.valueOf(pieceTypeString)
+
+        start = Field(startRow, startColumn)
+        end = Field(endRow, endColumn)
+    }
 }
