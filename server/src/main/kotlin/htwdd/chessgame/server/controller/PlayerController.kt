c11c878f0258d5d54fc91f637a8788338320483b
GagaMen <felixdimmel93@gmail.com>
2018-03-07 17:26:11 +0100

    Switch player controller actions from pultusORM to ORMLite + fix adding (post/put) see idempotency

--- server/src/main/kotlin/htwdd/chessgame/server/controller/PlayerController.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/controller/PlayerController.kt
@@ -1,74 +1,65 @@
 package htwdd.chessgame.server.controller
 
 import htwdd.chessgame.server.model.Player
-import ninja.sakib.pultusorm.core.PultusORM
-import ninja.sakib.pultusorm.core.PultusORMCondition
-import ninja.sakib.pultusorm.core.PultusORMUpdater
+import htwdd.chessgame.server.util.DatabaseUtility
 import org.springframework.web.bind.annotation.*
 
 @RestController
 class PlayerController {
-    private val pultusORM: PultusORM = PultusORM("chessgame.db")
+    private val playerDao = DatabaseUtility.playerDao
 
     @CrossOrigin(origins = ["http://localhost:63342"])
     @GetMapping("/player")
     fun getPlayerList(): HashMap<Int, Player> {
         val playerList = HashMap<Int, Player>()
-
-        pultusORM.find(Player())
-                .filterIsInstance<Player>()
-                .forEach { it ->
-                    playerList[it.id] = it
-                }
-
+        playerDao!!.queryForAll().forEach { playerList[it.id] = it }
         return playerList
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
     @GetMapping("/player/{id}")
     fun getPlayerById(@PathVariable id: Int): Any {
-        val condition = PultusORMCondition.Builder()
-                .eq("id", id)
-                .build()
-
-        pultusORM.find(Player(), condition)
-                .filterIsInstance<Player>()
-                .forEach { it ->
-                    return it
-                }
-
-        return "No player with id \"$id\" registered!"
+        return playerDao!!.queryForId(id) ?: return "No player with id \"$id\" registered!"
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
     @DeleteMapping("/player/{id}")
     fun deletePlayerById(@PathVariable id: Int): Boolean {
-        val condition = PultusORMCondition.Builder()
-                .eq("id", id)
-                .build()
-
-        return pultusORM.delete(Player(), condition)
+        if (playerDao!!.deleteById(id) != 1) return false
+        return true
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @PutMapping("/player")
+    @PostMapping("/player")
     fun addPlayer(@RequestParam name: String,
                   @RequestParam password: String): Boolean {
-        return pultusORM.save(Player(name, password))
+        if (playerDao!!.create(Player(name = name, password = password)) != 1) return false
+        return true
+    }
+
+    @CrossOrigin(origins = ["http://localhost:63342"])
+    @PutMapping("/player/{id}")
+    fun replacePlayer(@PathVariable id: Int,
+                      @RequestParam name: String,
+                      @RequestParam password: String): Boolean {
+        val player = playerDao!!.queryForId(id) ?: return false
+
+        player.name = name
+        player.password = password
+
+        if (playerDao.update(player) != 1) return false
+        return true
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
     @PatchMapping("/player/{id}")
     fun updatePlayer(@PathVariable id: Int,
                      @RequestParam password: String): Boolean {
-        val condition = PultusORMCondition.Builder()
-                .eq("id", id)
-                .build()
-        val updater = PultusORMUpdater.Builder()
-                .set("password", password)
-                .condition(condition)
-                .build()
+        val player = playerDao!!.queryForId(id) ?: return false
 
-        return pultusORM.update(Player(), updater)
+        player.password = password
+
+        if (playerDao.update(player) != 1) return false
+        return true
     }
 }
