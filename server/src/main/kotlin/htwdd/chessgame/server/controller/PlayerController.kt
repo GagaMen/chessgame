7aa63209edacb914df16c95b161683a7f70ec479
GagaMen <felixdimmel93@gmail.com>
2018-03-07 17:31:42 +0100

    Add OPTIONS request to player controller

--- server/src/main/kotlin/htwdd/chessgame/server/controller/PlayerController.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/controller/PlayerController.kt
@@ -3,13 +3,26 @@ package htwdd.chessgame.server.controller
 import htwdd.chessgame.server.model.Player
 import htwdd.chessgame.server.util.DatabaseUtility
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class PlayerController {
     private val playerDao = DatabaseUtility.playerDao
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @GetMapping("/player")
+    @RequestMapping("player", method = [RequestMethod.OPTIONS])
+    fun playerOptions(response: HttpServletResponse) {
+        response.setHeader("Allow", "HEAD,GET,POST,OPTIONS")
+    }
+
+    @CrossOrigin(origins = ["http://localhost:63342"])
+    @RequestMapping("player/{id}", method = [RequestMethod.OPTIONS])
+    fun playerByIdOptions(response: HttpServletResponse) {
+        response.setHeader("Allow", "HEAD,GET,PUT,PATCH,DELETE,OPTIONS")
+    }
+
+    @CrossOrigin(origins = ["http://localhost:63342"])
+    @GetMapping("player")
     fun getPlayerList(): HashMap<Int, Player> {
         val playerList = HashMap<Int, Player>()
         playerDao!!.queryForAll().forEach { playerList[it.id] = it }
@@ -17,20 +30,20 @@ class PlayerController {
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @GetMapping("/player/{id}")
+    @GetMapping("player/{id}")
     fun getPlayerById(@PathVariable id: Int): Any {
         return playerDao!!.queryForId(id) ?: return "No player with id \"$id\" registered!"
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @DeleteMapping("/player/{id}")
+    @DeleteMapping("player/{id}")
     fun deletePlayerById(@PathVariable id: Int): Boolean {
         if (playerDao!!.deleteById(id) != 1) return false
         return true
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @PostMapping("/player")
+    @PostMapping("player")
     fun addPlayer(@RequestParam name: String,
                   @RequestParam password: String): Boolean {
         if (playerDao!!.create(Player(name = name, password = password)) != 1) return false
@@ -38,7 +51,7 @@ class PlayerController {
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @PutMapping("/player/{id}")
+    @PutMapping("player/{id}")
     fun replacePlayer(@PathVariable id: Int,
                       @RequestParam name: String,
                       @RequestParam password: String): Boolean {
@@ -52,7 +65,7 @@ class PlayerController {
     }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
-    @PatchMapping("/player/{id}")
+    @PatchMapping("player/{id}")
     fun updatePlayer(@PathVariable id: Int,
                      @RequestParam password: String): Boolean {
         val player = playerDao!!.queryForId(id) ?: return false
