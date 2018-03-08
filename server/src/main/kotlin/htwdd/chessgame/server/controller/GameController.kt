85197071753b715ec23d57b98118fcd281e7dcab
GagaMen <felixdimmel93@gmail.com>
2018-03-08 16:21:28 +0100

    Switch game controller actions form pultusORM to ORMLite

--- server/src/main/kotlin/htwdd/chessgame/server/controller/GameController.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/controller/GameController.kt
@@ -1,29 +1,25 @@
 package htwdd.chessgame.server.controller
 
 import htwdd.chessgame.server.model.Draw
-import ninja.sakib.pultusorm.core.PultusORM
-import ninja.sakib.pultusorm.core.PultusORMCondition
+import htwdd.chessgame.server.util.DatabaseUtility
 import org.springframework.web.bind.annotation.*
+import javax.servlet.http.HttpServletResponse
 
 @RestController
 class GameController {
-    private val pultusORM: PultusORM = PultusORM("chessgame.db")
+    private val drawDao = DatabaseUtility.drawDao
+
+    @CrossOrigin(origins = ["http://localhost:63342"])
+    @RequestMapping("match/{id}/draw", method = [RequestMethod.OPTIONS])
+    fun drawsByMatchOptions(response: HttpServletResponse) {
+        response.setHeader("Allow", "HEAD,GET,OPTIONS")
+    }
 
     @CrossOrigin(origins = ["http://localhost:63342"])
     @GetMapping("match/{id}/draw")
-    fun getDrawsByMatchId(@PathVariable id: Int): MutableList<Draw> {
-        val drawList: MutableList<Draw> = mutableListOf()
-        val condition = PultusORMCondition.Builder()
-                .eq("id", id)
-                .build()
-
-        pultusORM.find(Draw(), condition)
-                .filterIsInstance<Draw>()
-                .forEach { draw ->
-                    draw.setValuesByDrawCode()
-                    drawList.add(draw)
-                }
-
+    fun getDrawsByMatchId(@PathVariable id: Int): Collection<Draw> {
+        val drawList = mutableListOf<Draw>()
+        drawDao!!.queryForEq("match_id", id).forEach { drawList.add(it) }
         return drawList
     }
 }
