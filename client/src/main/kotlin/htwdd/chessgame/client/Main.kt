15706b88b7a92e5dd61c02fd7546d6a0a24f1d23
GagaMen <felixdimmel93@gmail.com>
2018-04-25 17:36:21 +0200

    Remove pieceSet and draw request from loadData function in main file
    - This data are only necessary if a match was started
    - Improve Performance

--- client/src/main/kotlin/htwdd/chessgame/client/Main.kt
+++ client/src/main/kotlin/htwdd/chessgame/client/Main.kt
@@ -5,7 +5,6 @@ import htwdd.chessgame.client.controller.PlayerController
 import htwdd.chessgame.client.controller.StartController
 import htwdd.chessgame.client.model.Client
 import htwdd.chessgame.client.model.MatchHashMap
-import htwdd.chessgame.client.model.PieceSetHashMap
 import htwdd.chessgame.client.model.PlayerHashMap
 import htwdd.chessgame.client.util.RequestUtility.Companion.get
 import kotlinx.serialization.json.JSON
@@ -28,26 +27,10 @@ private fun loadData(): Client {
         }
     }
 
-    get("http://127.0.0.1:8080/match") {
+    get("http://127.0.0.1:8080/match", Pair("includePieceSets", false), Pair("includeHistory", false)) {
         if (it.target is XMLHttpRequest) {
             val matchHashMap = JSON.parse<MatchHashMap>((it.target as XMLHttpRequest).responseText)
-            matchHashMap.matches.forEach { (matchId, match) ->
-
-                get("http://127.0.0.1:8080/match/$matchId/pieceSets") {
-                    if (it.target is XMLHttpRequest) {
-                        val pieceSetHashMap = JSON.parse<PieceSetHashMap>((it.target as XMLHttpRequest).responseText)
-                        match.pieceSets = pieceSetHashMap.pieceSets
-                    }
-                }
-//                get("http://127.0.0.1:8080/match/$matchId/draw") {
-//                    if (it.target is XMLHttpRequest) {
-//                        val drawList = JSON.parse<DrawList>((it.target as XMLHttpRequest).responseText)
-//                        match.history = drawList.draws
-//                    }
-//                }
-
-                client.addMatch(match)
-            }
+            matchHashMap.matches.forEach { client.addMatch(it.value) }
         }
     }
 
