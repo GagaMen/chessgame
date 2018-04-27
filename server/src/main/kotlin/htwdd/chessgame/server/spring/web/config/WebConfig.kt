b55de640469a748d4982090c7d9ec3a6d0d578bb
GagaMen <felixdimmel93@gmail.com>
2018-04-27 09:25:27 +0200

    Add fasterxml library + webconfig for xml support (content negotiation)

---
+++ server/src/main/kotlin/htwdd/chessgame/server/spring/web/config/WebConfig.kt
@@ -0,0 +1,16 @@
+package htwdd.chessgame.server.spring.web.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.MediaType
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+
+@Configuration
+@EnableWebMvc
+class WebConfig : WebMvcConfigurerAdapter() {
+
+    override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer?) {
+        configurer!!.favorPathExtension(true).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(false).useJaf(false).defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML).mediaType("json", MediaType.APPLICATION_JSON)
+    }
+}
