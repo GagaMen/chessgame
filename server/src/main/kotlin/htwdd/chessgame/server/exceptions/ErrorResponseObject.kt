37df6f22a12e5662815153705e2bb4e9f7cb7650
GagaMen <felixdimmel93@gmail.com>
2018-04-27 10:25:23 +0200

    Prepare error response object for xml parsing
    - This is necessary if the client wants to get back XML

--- server/src/main/kotlin/htwdd/chessgame/server/exceptions/ErrorResponseObject.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/exceptions/ErrorResponseObject.kt
@@ -2,14 +2,26 @@ package htwdd.chessgame.server.exceptions
 
 import org.springframework.http.HttpStatus
 import java.util.*
+import javax.xml.bind.annotation.XmlAccessType
+import javax.xml.bind.annotation.XmlAccessorType
+import javax.xml.bind.annotation.XmlElement
+import javax.xml.bind.annotation.XmlRootElement
 
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
 class ErrorResponseObject(
-        val timestamp: Date,
-        val statusCode: Int,
-        val error: HttpStatus,
-        val exception: String,
-        val message: String,
-        val path: String
+        @XmlElement
+        val timestamp: Date = Date(),
+        @XmlElement
+        val statusCode: Int = 500,
+        @XmlElement
+        val error: HttpStatus = HttpStatus.INTERNAL_SERVER_ERROR,
+        @XmlElement
+        val exception: String = "",
+        @XmlElement
+        val message: String = "No message available",
+        @XmlElement
+        val path: String = ""
 ) {
 
     override fun toString(): String {
