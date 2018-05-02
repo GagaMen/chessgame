d9f2922531aa0e9d0e3d7a78ee927e5cfc70a2e6
GagaMen <felixdimmel93@gmail.com>
2018-05-02 11:07:25 +0200

    Prepare server for deploying with static resources
    - set chessgame.db file location to root
    - extend server application from SpringBootServletInitialzer for
      supporting static resources
    - add config to provide static resources on root entry point

--- server/src/main/kotlin/htwdd/chessgame/server/spring/web/config/WebConfig.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/spring/web/config/WebConfig.kt
@@ -4,7 +4,9 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.http.MediaType
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.EnableWebMvc
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
+import org.springframework.web.servlet.resource.PathResourceResolver
 
 @Configuration
 @EnableWebMvc
@@ -12,5 +14,14 @@ class WebConfig : WebMvcConfigurerAdapter() {
 
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer?) {
         configurer!!.favorPathExtension(true).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(false).useJaf(false).defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML).mediaType("json", MediaType.APPLICATION_JSON)
+    }
+
+    override fun addResourceHandlers(registry: ResourceHandlerRegistry?) {
+        registry!!
+                .addResourceHandler("/**")
+                .addResourceLocations("classpath:/resources/", "classpath:/META-INF/resources/", "classpath:/static/", "classpath:/public/")
+                .setCachePeriod(3600)
+                .resourceChain(true)
+                .addResolver(PathResourceResolver())
     }
 }
