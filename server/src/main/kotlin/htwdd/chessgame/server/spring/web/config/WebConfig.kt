89b1d1eeabe0dded833eccbccec02796d8511b84
GagaMen <felixdimmel93@gmail.com>
2018-05-25 17:40:28 +0200

    Add document header for spring configuration

--- server/src/main/kotlin/htwdd/chessgame/server/spring/web/config/WebConfig.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/spring/web/config/WebConfig.kt
@@ -5,9 +5,17 @@ import org.springframework.http.MediaType
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
+/**
+ * Spring web configuration object
+ */
 @Configuration
 class WebConfig : WebMvcConfigurerAdapter() {
-
+    /**
+     * Configuration for content negotiation (3 strategies)
+     * 1. URL suffix strategy
+     * 2. URL parameter strategy
+     * 3. Accept header strategy
+     */
     override fun configureContentNegotiation(configurer: ContentNegotiationConfigurer?) {
         configurer!!.favorPathExtension(true).favorParameter(true).parameterName("mediaType").ignoreAcceptHeader(false).useJaf(false).defaultContentType(MediaType.APPLICATION_JSON).mediaType("xml", MediaType.APPLICATION_XML).mediaType("json", MediaType.APPLICATION_JSON)
     }
