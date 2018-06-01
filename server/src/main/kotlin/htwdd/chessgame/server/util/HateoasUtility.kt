f1009d6ca6698b42ade6aa795edd75e75da7f83e
GagaMen <felixdimmel93@gmail.com>
2018-06-01 11:42:27 +0200

    Add documentation header for hateoas utility

--- server/src/main/kotlin/htwdd/chessgame/server/util/HateoasUtility.kt
+++ server/src/main/kotlin/htwdd/chessgame/server/util/HateoasUtility.kt
@@ -3,8 +3,25 @@ package htwdd.chessgame.server.util
 import org.springframework.hateoas.Link
 import org.springframework.http.HttpHeaders
 
+/**
+ * Utility class to handle creation of link header for HATEOAS support
+ *
+ * @author Felix Dimmel
+ *
+ * @since 1.0.0
+ */
 class HateoasUtility {
+    /**
+     * Static HateoasUtility object
+     */
     companion object {
+        /**
+         * Creates the link header by a list of links
+         *
+         * @param linkList Contains a list of links
+         *
+         * @return HttpHeaders object with set link header
+         */
         fun createLinkHeader(linkList: HashSet<Pair<Link, String>>): HttpHeaders {
             val headers = HttpHeaders()
             val sb = StringBuilder()
